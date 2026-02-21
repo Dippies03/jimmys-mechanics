f8f7a4646b3267e3243b3f5be87983930c7cfa77
Dippies03 <31156355+Dippies03@users.noreply.github.com>
2026-02-21 19:23:43 +0200

    Create config.js

---
+++ assets/clients/jimmys-mechanics/assets/config.js
@@ -0,0 +1,13 @@
+// IMPORTANT:
+// If your GitHub Pages site is served from a repo path like:
+// https://dippies03.github.io/CPT-Plumber-Example/
+// then BASE_PATH should be "/CPT-Plumber-Example"
+// If it's served from a user/org root like:
+// https://dippies03.github.io/
+// then BASE_PATH should be ""
+
+window.SITE_CONFIG = {
+  BASE_PATH: "",
+  CONTENT_API_URL: "https://script.google.com/macros/s/AKfycbx1Nvj0FFhgFwKtAenBKRVOJQzJzlH_Og3XTpn0ZVTfbPHTaHHjeYl-ruGfBs7Phma5RQ/exec",
+  THANK_YOU_PATH: "/thanks.html"
+};
